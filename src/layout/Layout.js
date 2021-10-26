fb39e6f9b00a7b014ab2cff39ed2959756f41973
CeAlCuadrado <cealcuadrado@outlook.com>
2021-10-26 18:46:24 -0300

    Aplicado ruteo al layout

--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -1,7 +1,38 @@
+import Blogs from '../pages/blogs/Blogs';
+import Contact from '../pages/contact/Contact';
+import Home from '../pages/home/Home';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
 function Layout() {
-    return (
-        <p>Hola, mundo!</p>
-    );
+  return (
+    <div class="layout">
+      <Router>
+        <div>
+          <Link to="/">Home</Link>
+        </div>
+        <div>
+          <Link to="/blogs">Blogs</Link>
+        </div>
+        <div>
+          <Link to="/contact">Contact</Link>
+        </div>
+
+        <hr />
+
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/blogs">
+            <Blogs />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 }
 
 export default Layout;
